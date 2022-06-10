3d5e2bf8e9c06bb420ab49a025f2e981da065a2e
Prathap <prathap.sims@gmail.com>
2022-06-10 03:26:29 +0000

    Calling function-hello.js

---
+++ hello.js
@@ -0,0 +1,25 @@
+
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+
+// const sayHello  = function (name) {
+//   console.log("Hello, " + name);
+// }
+
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+const sayHello  = function (name) {
+  return "Hello, " + name
+}
+
+console.log(sayHello("Caliban"));
+
+var miranda = sayHello("Miranda");
+console.log(miranda);
+
